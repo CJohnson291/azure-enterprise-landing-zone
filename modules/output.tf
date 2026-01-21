453cf00d6846be42afc774d25e1a000cce31672c
chris johnson <chrisjohnson@chriss-MacBook-Pro.local>
2026-01-21 13:18:10 +0000

    Day 4: Add policy module and assign allowed locations policy

---
+++ modules/output.tf
@@ -0,0 +1,11 @@
+output "platform_mg_id" {
+  value = azurerm_management_group.platform.id
+}
+
+output "corp_mg_id" {
+  value = azurerm_management_group.corp.id
+}
+
+output "sandbox_mg_id" {
+  value = azurerm_management_group.sandbox.id
+}
